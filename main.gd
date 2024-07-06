a786e1e7d82f4e29916c611c5f139689a4ee2606
Dung Dinh <57259604+dqdthanhthanh@users.noreply.github.com>
2024-07-06 10:33:02 +0700

    updates script

--- main.gd
+++ main.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 # I HAVE ADDED KEY ALSO BUT DON'T WORRY I AM GOING TO CHANGE KEY
 onready var http = $HTTPRequest
-var sheetname = "RESULTS"
 var date = ""
 var time = ""
 var cate = ""
@@ -10,15 +9,16 @@ var amount = ""
 var desc = ""
 var respheader = ""
 var SEND = false
-var apiurl = "https://script.google.com/macros/s/AKfycbysOVA-CmX8jrISj1Tl39ChQJs0vzpYKa2ksde6iLq_WqblQeFKA-Mp9AQ5eMdh9PImyQ/exec"
-var geturl = apiurl+"?sheetname="+sheetname
+var excel_file = "https://docs.google.com/spreadsheets/d/1BxOynf2MqJURThmiUzzOts4i360y1pUliJSAkjYljAE/edit?gid=0#gid=0"
+var idurl = "AKfycbyAZP-lPX5xgmj_BrxQ96GmPXB_1dKylRE9PhH1UcdYpBO68F01IFI-7VzbLvPHW205lg"
+var apiurl = "https://script.google.com/macros/s/" +idurl+ "/exec"
 
 func _ready():
 	getdata()
 	
 func getdata():
-	$Label.text = "Fetching: "+sheetname
-	$HTTPRequest.request(geturl)
+	$Label.text = "Fetching: "+"RESULTS"
+	$HTTPRequest.request(apiurl+"?sheetname="+"RESULTS")
 	SEND = false
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
@@ -27,15 +27,16 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	else:
 		respheader = headers
 		if response_code == 200:
-			$Label.text = "Fetched: "+sheetname
+			$Label.text = "Fetched: "+"RESULTS"
+			var json = JSON.parse(body.get_string_from_utf8())
+			print(json.result)
 			var data = body.get_string_from_utf8()
 			data = parse_json(data)[0]
-#			print(data)
 			$CASH_IN_HAND.text = "CASH IN HAND:  "+str(data["TOTAL_CASH_IN_HAND"])
 			$TOTAL_INCOME.text = "TOTAL INCOME:  "+str(data["TOTAL_INCOME"])
 			$TOTAL_EXPENSE.text = "TOTAL EXPENSE: "+str(data["TOTAL_EXPENSE"])
 		else:
-			print(response_code)
+			prints('response_code',response_code)
 			$Label.text = "Trying again response code: "+str(response_code)
 			getdata()
 
@@ -49,6 +50,7 @@ func _on_SEND_pressed():
 	desc = str($DESC.text)
 	
 	var datasend = "?date="+date+"&time="+time+"&cate="+cate+"&amount="+amount+"&desc="+desc+"&sheetname="+"DATA"
+	prints(datasend)
 	var headers = ["Content-Length: 0"]
 	var posturl = apiurl+datasend
 	$HTTPRequest.request(posturl,headers,true, HTTPClient.METHOD_POST)
